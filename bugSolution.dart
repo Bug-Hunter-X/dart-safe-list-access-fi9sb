9e5dd39df097f9bbef45f38aa800940e71e2497a
BugHunter-X <askzenai@gmail.com>
2024-12-05 16:00:06 +0100

    Add bugSolution.dart file

---
+++ bugSolution.dart
@@ -0,0 +1,10 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int index = 6;
+
+if (index >= 0 && index < numbers.length) {
+  print(numbers[index]);
+} else {
+  print("Index out of range!");
+}
+```
